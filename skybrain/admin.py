dd38d47bd9a8b6d1a4b1f3604b2de1a288b54b8d
Faisal <faisalbashir353@gmail.com>
2023-06-20 10:03:27 +0530

    ***CHANGES 1. ms

--- skybrain/admin.py
+++ skybrain/admin.py
@@ -1,23 +1,23 @@
-from django.contrib import admin
-from skybrain.models import Organization, LicensesRequests, Room, RoomHistory, RoomItems, CustomerSupport
-
-
-class RoomHistoryAdmin(admin.ModelAdmin):
-    list_display = ("room", "user_input", "added_on")
-
-
-class RoomItemsAdmin(admin.ModelAdmin):
-    list_display = ("room", "name", "price", "category", "is_private", "added_on")
-    list_filter = ("room__organization", )
-
-
-class CustomerSupportAdmin(admin.ModelAdmin):
-    list_display = ("room", "user_input", "has_replied", "added_on")
-
-
-admin.site.register(Organization)
-admin.site.register(LicensesRequests)
-admin.site.register(Room)
-admin.site.register(CustomerSupport, CustomerSupportAdmin)
-admin.site.register(RoomItems, RoomItemsAdmin)
-admin.site.register(RoomHistory, RoomHistoryAdmin)
+# from django.contrib import admin
+# from skybrain.models import Organization, LicensesRequests, Room, RoomHistory, RoomItems, CustomerSupport
+#
+#
+# class RoomHistoryAdmin(admin.ModelAdmin):
+#     list_display = ("room", "user_input", "added_on")
+#
+#
+# class RoomItemsAdmin(admin.ModelAdmin):
+#     list_display = ("room", "name", "price", "category", "is_private", "added_on")
+#     list_filter = ("room__organization", )
+#
+#
+# class CustomerSupportAdmin(admin.ModelAdmin):
+#     list_display = ("room", "user_input", "has_replied", "added_on")
+#
+#
+# admin.site.register(Organization)
+# admin.site.register(LicensesRequests)
+# admin.site.register(Room)
+# admin.site.register(CustomerSupport, CustomerSupportAdmin)
+# admin.site.register(RoomItems, RoomItemsAdmin)
+# admin.site.register(RoomHistory, RoomHistoryAdmin)
--- skybrain/serializers.py
+++ skybrain/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 
-from skybrain.models import LicensesRequests, Room, RoomHistory, RoomItems, Organization, CustomerSupport
+# from skybrain.models import LicensesRequests, Room, RoomHistory, RoomItems, Organization, CustomerSupport
 
 
-class LicensesViewSerializer(serializers.ModelSerializer):
-    no_of_licenses = serializers.IntegerField(required=True, min_value=1)
-
-    class Meta:
-        model = LicensesRequests
-        exclude = (
-            "added_on",
-        )
+# class LicensesViewSerializer(serializers.ModelSerializer):
+#     no_of_licenses = serializers.IntegerField(required=True, min_value=1)
+#
+#     class Meta:
+#         model = LicensesRequests
+#         exclude = (
+#             "added_on",
+#         )
 
 
 class CreateLicensesViewSerializer(serializers.Serializer):
@@ -19,23 +19,23 @@ class CreateLicensesViewSerializer(serializers.Serializer):
     csv_file = serializers.FileField(required=True)
 
 
-class OrganizationRoomsSerializer(serializers.ModelSerializer):
-    organization_name = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = Room
-        exclude = (
-            "room_key",
-            "organization",
-            "id",
-            "added_on",
-        )
-        read_only_fields = (
-            "organization_name",
-        )
-
-    def get_organization_name(self, room):
-        return room.organization.name
+# class OrganizationRoomsSerializer(serializers.ModelSerializer):
+#     organization_name = serializers.SerializerMethodField(read_only=True)
+#
+#     class Meta:
+#         model = Room
+#         exclude = (
+#             "room_key",
+#             "organization",
+#             "id",
+#             "added_on",
+#         )
+#         read_only_fields = (
+#             "organization_name",
+#         )
+#
+#     def get_organization_name(self, room):
+#         return room.organization.name
 
 
 class SkybrainCustomerRoomSerializer(serializers.Serializer):
@@ -44,44 +44,44 @@ class SkybrainCustomerRoomSerializer(serializers.Serializer):
     room_key = serializers.CharField(required=True)
 
 
-class HistoryRoomSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = RoomHistory
-        exclude = (
-            "id",
-            "room"
-        )
-
-
-class ItemsRoomViewSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(required=True)
-    video = serializers.FileField(required=True)
-    price = serializers.IntegerField(required=True)
-    description = serializers.CharField(required=True)
-    is_private = serializers.BooleanField(required=True)
-    name = serializers.CharField(required=True)
-
-    class Meta:
-        model = RoomItems
-        exclude = (
-            "id",
-            "added_on",
-        )
+# class HistoryRoomSerializer(serializers.ModelSerializer):
+#
+#     class Meta:
+#         model = RoomHistory
+#         exclude = (
+#             "id",
+#             "room"
+#         )
 
 
-class OrganizationsviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Organization
-        exclude = (
-            "id",
-            "added_on"
-        )
+# class ItemsRoomViewSerializer(serializers.ModelSerializer):
+#     image = serializers.ImageField(required=True)
+#     video = serializers.FileField(required=True)
+#     price = serializers.IntegerField(required=True)
+#     description = serializers.CharField(required=True)
+#     is_private = serializers.BooleanField(required=True)
+#     name = serializers.CharField(required=True)
+#
+#     class Meta:
+#         model = RoomItems
+#         exclude = (
+#             "id",
+#             "added_on",
+#         )
 
 
-class CSQueriesViewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomerSupport
-        exclude = (
-            "room",
-        )
+# class OrganizationsviewSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Organization
+#         exclude = (
+#             "id",
+#             "added_on"
+#         )
+#
+#
+# class CSQueriesViewSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = CustomerSupport
+#         exclude = (
+#             "room",
+#         )
