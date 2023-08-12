f3134a503b4d7b4fab90f64aaad6757a61d68f9c
Faisal <faisalbashir353@gmail.com>
2023-08-12 13:00:40 +0530

    ***CHANGES 1. ms

--- skybrain/views.py
+++ skybrain/views.py
@@ -154,8 +154,9 @@ class ValidateRoom(generics.ListAPIView):
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 1000
+    page_size = 100
     page_size_query_param = 'page_size'
+    max_page_size = 100
 
 
 class HistoryRoom(generics.ListAPIView):
