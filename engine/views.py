fca156ed22eaacc4b36edba0739317e0b9ccc035
Faisal Ahmad <faisal.ahmad@doctustech.com>
2024-07-17 22:33:11 +0530

    **CHANGES 1. community changes

--- engine/views.py
+++ engine/views.py
@@ -1218,7 +1218,6 @@ class WhatsappWebhook(generics.ListCreateAPIView):
             return response
 
     def process_whatsapp_message(self, body):
-        InternalExceptions.objects.create(text=body)
         wa_id = body["entry"][0]["changes"][0]["value"]["contacts"][0]["wa_id"]
         name = body["entry"][0]["changes"][0]["value"]["contacts"][0]["profile"]["name"]
 
@@ -1228,7 +1227,6 @@ class WhatsappWebhook(generics.ListCreateAPIView):
             message_body = message["image"]["caption"]
             self.get_media(message["image"]["id"], message_body, wa_id, client_phone_no, name)
         except KeyError:
-            InternalExceptions.objects.create(text=message)
             message_body = message["text"]["body"]
             data = self.get_text_message_input(wa_id, message)
             self.send_message(data, message_body, client_phone_no, name)
