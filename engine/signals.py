2eece8d4fbab2f35ba46784c8c93512ccb3fd0d8
Faisal Ahmad <faisal.ahmad@doctustech.com>
2024-07-17 21:33:49 +0530

    **CHANGES 1. community changes

--- engine/signals.py
+++ engine/signals.py
@@ -7,20 +7,20 @@ from engine.utils import upload_new_wechat_listing, \
     create_room_and_china_user, send_wechat_room_reply, send_login_otp_reply
 
 
-# @receiver(post_save, sender=WechatMessages)
-# def wechat_message_recieved(sender, instance, created, **kwargs):
-#     create_room_and_china_user(instance.wechat_id, instance)
-#     if instance.msg_type == "text" and instance.text.upper() == "ROOM LOGIN":
-#         send_login_otp_reply(instance)
-#     else:
-#         url = instance.pic_url
-#         run_in_thread(upload_new_wechat_listing, (url, instance))
-#
-#
-# @receiver(post_save, sender=VoiceCommands)
-# def voice_command_recieved(sender, instance, created, **kwargs):
-#     data = "ON" if instance.switch else "OFF"
-#     VoiceCommandsHistory.objects.create(
-#         voice=instance, data=data
-#     )
+@receiver(post_save, sender=WechatMessages)
+def wechat_message_recieved(sender, instance, created, **kwargs):
+    create_room_and_china_user(instance.wechat_id, instance)
+    if instance.msg_type == "text" and instance.text.upper() == "ROOM LOGIN":
+        send_login_otp_reply(instance)
+    else:
+        url = instance.pic_url
+        run_in_thread(upload_new_wechat_listing, (url, instance))
+
+
+@receiver(post_save, sender=VoiceCommands)
+def voice_command_recieved(sender, instance, created, **kwargs):
+    data = "ON" if instance.switch else "OFF"
+    VoiceCommandsHistory.objects.create(
+        voice=instance, data=data
+    )
 
