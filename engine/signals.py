e3c7022428a101aaff1211a8bfeebeeece9e783a
Faisal Ahmad <faisal.ahmad@doctustech.com>
2024-09-22 16:53:02 +0530

    **CHANGES 1. community changes

--- engine/signals.py
+++ engine/signals.py
@@ -1,7 +1,7 @@
 import requests
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from engine.models import Chatbots, WechatMessages, KeyManagement, VoiceCommandsHistory, VoiceCommands
+from engine.models import Chatbots, WechatMessages, KeyManagement, VoiceCommandsHistory, VoiceCommands, Items
 from engine.thread_functions import run_in_thread
 from engine.utils import upload_new_wechat_listing, \
     create_room_and_china_user, send_wechat_room_reply, send_login_otp_reply
@@ -23,4 +23,35 @@ def voice_command_recieved(sender, instance, created, **kwargs):
     VoiceCommandsHistory.objects.create(
         voice=instance, data=data
     )
+
+
+@receiver(post_save, sender=Items)
+def items_created(sender, instance, created, **kwargs):
+    try:
+        # if created:
+        data = {
+             'label': instance.title,
+             'ref': str(instance.item_id),
+             'price': instance.price,
+             'description': instance.description,
+             'stock': instance.stock,
+             'image_url': instance.qr_code.url
+        }
+
+        headers = {
+            'Accept': 'application/json',
+            'DOLAPIKEY': '79RdTE6zwkW8PxRD15099A4ecKqrCaqE'
+        }
+
+        url = 'http://plastireai.com/api/index.php/products'
+        response = requests.post(url, json=data, headers=headers)
+        print(response.json())
+        # dolibarr.Dolibarr.call_create_api()
+
+    except Exception as e:
+        print(e)
+
+
+
+
 
